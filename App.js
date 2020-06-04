01d59f7da28b1c3d0b6dbc2f9bf08568ea6f63f3
Bryan Chua <me@bryanchua.com>
2020-06-04 14:20:00 +0800

    upgrade expo and packages due to deprecated packages

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat } from 'react-native-gifted-chat';
 import Amplify, { Analytics, Interactions } from 'aws-amplify';
 import aws_exports from './aws-exports';
 
 Amplify.configure(aws_exports);
 
-const botName = "BookTrip"
+const botName = 'BookTrip';
 const botUser = {
   _id: 2,
   name: 'React Native',
-  avatar: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+  avatar:
+    'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
 };
 let chatId = 1;
 
@@ -19,15 +20,16 @@ export default class App extends React.Component {
     messages: [
       {
         _id: chatId,
-        text: "Hello developer, this is a BookTrip Lex Chatbot. Try \"I want to reserve a hotel for tonight\"",
+        text:
+          'Hello developer, this is a BookTrip Lex Chatbot. Try "I want to reserve a hotel for tonight"',
         user: botUser,
         createdAt: new Date(),
-      }
-    ]
+      },
+    ],
   };
 
   componentDidMount() {
-    Analytics.record('componentDidMount')
+    Analytics.record('componentDidMount');
   }
 
   sendMessageToBot = async (userInput) => {
@@ -35,9 +37,9 @@ export default class App extends React.Component {
     const response = await Interactions.send(botName, userInput);
 
     // Log chatbot response
-    this.appendChatMessages([this.formatMessage(response.message)])
-    Analytics.record('sendMessageToBot')
-  }
+    this.appendChatMessages([this.formatMessage(response.message)]);
+    Analytics.record('sendMessageToBot');
+  };
 
   formatMessage = (message) => {
     chatId = chatId + 1;
@@ -45,22 +47,22 @@ export default class App extends React.Component {
       _id: chatId,
       createdAt: new Date(),
       text: message,
-      user: botUser
-    }
-  }
+      user: botUser,
+    };
+  };
 
   onSend = (messages) => {
-    console.log(messages)
-    messages.map(msg => this.sendMessageToBot(msg.text))
-    this.appendChatMessages(messages)
-  }
+    console.log(messages);
+    messages.map((msg) => this.sendMessageToBot(msg.text));
+    this.appendChatMessages(messages);
+  };
 
   appendChatMessages = (messages) => {
-    console.log(messages)
-    this.setState(previousState => ({
+    console.log(messages);
+    this.setState((previousState) => ({
       messages: GiftedChat.append(previousState.messages, messages),
-    }))
-  }
+    }));
+  };
 
   render() {
     // Try "I want to reserve a hotel for tonight"
@@ -68,7 +70,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <GiftedChat
           messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
+          onSend={(messages) => this.onSend(messages)}
           user={{
             _id: 1,
           }}
